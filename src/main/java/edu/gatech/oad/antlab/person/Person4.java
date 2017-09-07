813c39b6b0eb789d338461d39dca2ffdbccd5f4a
Mason Buchanan <sbuchanan8@gatech.edu>
2017-09-07 14:33:50 -0400

    Changed Person 4 calc method

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -5,7 +5,7 @@ package edu.gatech.oad.antlab.person;
  *  returns their name and a
  *  modified string 
  *  
- *  @author Bob
+ *  @author Mason Buchanan
  *  @version 1.1
  */
 public class Person4 {
@@ -30,8 +30,13 @@ public class Person4 {
      * @return the modified string
      */
     private String calc(String input) {
-      //Person 4 put your implementation here
-      return null;
+        //Person 4 put your implementation here
+        String str = "";
+        for (int i = 0; i < input.length(); i++) {
+            char shift = (char) (input.charAt(i) + 1);
+            str += shift;
+        }
+        return str;
     }
     
     /**
